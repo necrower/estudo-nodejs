3a826eb5b907391165dac54cd3e5cf4688936118
Necrower Proed <necrower@gmail.com>
2016-06-30 18:34:43 -0300

    JWT
    Não está bonito, nem era para estar rs

--- 01_node-http-jwt/server.js
+++ 01_node-http-jwt/server.js
@@ -1,51 +1,92 @@
 var http = require('http');
-var jwt = require('./my-jwt');
-//var CryptoJS = require("crypto-js");
+var jwt = require('jsonwebtoken');
 
 //http://www.jonathan-petitcolas.com/2014/11/27/creating-json-web-token-in-javascript.html
+// {"login":"necrower","pass":"123456" }
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibG9naW4iOiJuZWNyb3dlciIsImlhdCI6MTQ2NzMyMTYyN30.z-OjESWwZRGCKEsPfehNoLPS63ygxZjpVaesGVarZLk
 
 var secret = "S3CR3T";
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response) {
 
-    if (request.method === 'GET') {
-        if(request.url === '/'){
-          response.writeHead(200, {"Content-Type" : "textplain"});
-          response.end("Hello!!");
-        } else if (request.url === '/protected'){
-            response.writeHead(403, {"Content-Type" : "textplain"});
-            response.end("Not Authorized");
-        } else {
-            routeNotFound(response);
+  var type = "text/plain";
+
+  if (request.method === 'GET') {
+    if (request.url === '/') {
+      response.writeHead(200, {
+        "Content-Type": type
+      });
+      response.end("Hello!!");
+
+    } else if (request.url === '/protected') {
+        var token = request.headers.authorization;
+
+        try {
+            var decoded = jwt.verify(token, secret);
+            response.writeHead(200, { "Content-Type": type });
+            response.end(JSON.stringify(decoded));
+        } catch(err) {
+            routeProtected(response);
         }
-    } else if(request.method === 'POST'){
-        if(request.url === '/login'){
-            response.writeHead(200, {"Content-Type" : "textplain"});
-            response.end("Hello!!");
-        } else {
-            routeNotFound(response);
-        }
+
     } else {
-        routeNotFound(response);
+      routeNotFound(response);
     }
+  } else if (request.method === 'POST') {
+    if (request.url === '/login') {
+        var body = '';
+        request.on('data', function(data) {
+          body += data;
+        });
+        request.on('end', function() {
+            var params = JSON.parse(body);
+
+            var credencials = {
+                login: params.login,
+                password: params.pass
+            };
+
+            //simulando o retorno do usuário
+            var user = {
+                id : 1,
+                login : credencials.login
+            }
+
+            var ret = '';
+            if(user && user.login && user.login === 'necrower')
+                 ret = JSON.stringify({ "token" : jwt.sign(user, secret)})
+
+            response.writeHead(200, { "Content-Type": "application/json" });
+            response.end(ret);
+
+        });
+
+
+
+
+
+    } else {
+      routeNotFound(response);
+    }
+  } else {
+    routeNotFound(response);
+  }
 
   //  response.end("WORKING! ");
 });
 
-function routeNotFound(response){
-    response.writeHead(404, {"Content-Type" : "textplain"});
-    response.end("Not Found");
+function routeNotFound(response) {
+  response.writeHead(404, {
+    "Content-Type": "textplain"
+  });
+  response.end("Not Found");
 }
 
-
-
-//console.log(console.log(CryptoJS.HmacSHA1("Message", "Key")));
-//console.log(helpers.createUnsignedToken());
-var tk = jwt.createSigned({name: "necrower"}, secret);
-//console.log(tk);
-
-console.log(jwt.decode(tk));
-
-
+function routeProtected(response) {
+  response.writeHead(403, {
+    "Content-Type": "textplain"
+  });
+  response.end("Not Authorized");
+}
 
 server.listen(3000);
