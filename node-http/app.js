8dce61e78ab266ebd34530919c193e9497eeb66c
Mario Junior <necrower@gmail.com>
2016-06-30 00:14:31 -0300

    Incrementando

--- node-http/app.js
+++ node-http/app.js
@@ -1,7 +1,25 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
-    response.end("Funciona!");
+
+    switch (request.url) {
+        case "/":
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.end("<h1>Show time!</h1>");
+            break;
+        case "/about":{
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.end("<h1>Mario Costa</h1><br /><h3>@necrower</h3>");
+            break;
+
+        } default: {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.end("<h1> :( </h1>");
+        }
+
+    }
+
+
 });
 
 server.listen(3000);
